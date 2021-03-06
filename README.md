62ea9687e75d79300907dbeba2591ba09c17300c
KV Teja <vishnu.koyya@glarimy.com>
2021-03-07 01:22:47 +0530

    final version

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# react-spacex
+#SpaceX Dashboard
+Vishnu Teja Koyya
+vishnu.koya@gmail.com
+6 MARCH 2021
+
+##Features
+
+1. User registration
+2. User login
+3. User logout
+4. Display of all launches
+5. Display of past launches
+6. Display of upcoming launches
+7. Display of launches between two dates
+8. Display of launch detail
+9. Responsive UI
+10. Input Validation
+
+##Sample screens
+
+Registration/Login screen
+![](login.jpg)
+
+Launch listing
+![](list.jpg)
+
+Launch detail
+![](detail.jpg)
+
+##Areas for improvement
+
+1. Infinite scrolling
+2. Validation of dates
+
+##Technology
+
+1. ReactJS
+2. Redux
+3. Firebase
+4. HTML & CSS
+5. Fetch
--- src/components/Form/FormView.js
+++ src/components/Form/FormView.js
@@ -6,9 +6,11 @@ const FormView = props => {
         <div className={`formBg ${props.className}`}>
             <div className="main-title">SpaceX Dashboard</div>
             <div>
-                <button className="button" onClick={props.allLaunches}>All launches</button>
-                <button className="button" onClick={props.pastLaunches}>Previous launches</button>
-                <button className="button" onClick={props.upComingLaunches}>Upcoming launches</button>
+                <div className="nav">
+                    <button className="button" onClick={props.allLaunches}>All launches</button>
+                    <button className="button" onClick={props.pastLaunches}>Previous launches</button>
+                    <button className="button" onClick={props.upComingLaunches}>Upcoming launches</button>
+                </div>
                 <div className="fliter">
                     <input className="input" type="date" value={props.startDate} onChange={props.onStartDate}></input>
                     <input className="input" type="date" value={props.endDate} onChange={props.onEndDate}></input>
